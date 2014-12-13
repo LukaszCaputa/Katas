8cd03926d719e4ad89576379658795b58acb8ed4
LukaszCaputa <lukasz@lukaszcaputa.eu>
2014-12-13 17:25:29 +0100

    Bowling game Kata
    completed implementation of bowling game kata - by Rober C. Martin

---
+++ bowlingGameDojo/src/eu/lukaszcaputa/Game.java
@@ -0,0 +1,39 @@
+package eu.lukaszcaputa;
+
+public class Game {
+	
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
+
+	public void roll(int num){
+		rolls[currentRoll++] = num;
+	}
+	
+	public int score(){
+		int score = 0;
+		int frameIndex = 0;
+		
+		for(int frame = 0; frame <10 ; frame++){
+			if(isStrike(frameIndex)){
+				score += 10 + rolls[frameIndex+1] + rolls[frameIndex+2];
+				frameIndex++;
+			}else if(isSpare(frameIndex)){
+				score += 10 + rolls[frameIndex+2];
+				frameIndex+=2;
+
+			}else{
+				score += rolls[frameIndex] + rolls[frameIndex+1];
+				frameIndex+=2;
+
+			}
+		}
+		return score;
+	}
+	
+	private boolean isSpare(int frameIndex){
+		return rolls[frameIndex] + rolls[frameIndex+1]==10 ;
+	}
+	private boolean isStrike(int frameIndex){
+		return rolls[frameIndex]==10;
+	}
+}
